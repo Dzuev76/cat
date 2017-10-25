9aa4052c59115e0045bda78f49e8594729c9e4f9
Dzuev76 <Dzuev76@gmail.com>
2017-10-25 12:02:12 +0600

    change

--- js/fadein.js
+++ js/fadein.js
@@ -1,10 +1,19 @@
 $(document).ready(function(){
-  	$("h1").fadeOut(5000).fadeIn(5000);
+  	$('h1').fadeOut(5000).fadeIn(5000);
       });
-
-jQuery(function(f){
-    var element = f('.card');
-    f(window).scroll(function(){
-        element['fade'+ (f(this).scrollTop() > 200 ? 'In': 'Out')](1500);          
-    });
+// jQuery(function(f){
+//     var element = f('.card');
+//     f(window).scroll(function(){
+//         element['fade'+ (f(this).scrollTop() > 200 ? 'In': 'Out')](2000);           
+//     });
+// });
+$(document).ready(function(){
+	$(window).scroll(function(){
+		if($(window).scrollTop()>120){
+			$('.card,.media').fadeIn(3000)
+		}else{
+			$('.card,.media').fadeOut(1700)
+		}
+	});
 });
+
