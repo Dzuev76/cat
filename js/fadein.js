e3004c0be8fb35afeaa185af5ad626c23d3ebd50
Dzuev76 <Dzuev76@gmail.com>
2017-10-25 09:30:30 +0600

    change

--- js/fadein.js
+++ js/fadein.js
@@ -2,3 +2,9 @@ $(document).ready(function(){
   	$("h1").fadeOut(5000).fadeIn(5000);
       });
 
+jQuery(function(f){
+    var element = f('.card');
+    f(window).scroll(function(){
+        element['fade'+ (f(this).scrollTop() > 200 ? 'In': 'Out')](1500);          
+    });
+});
